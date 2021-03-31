914d86a49fc6f6f8bf78d6bf90e2627be2e19248
odrevet <odrevet@gmail.com>
2021-03-31 21:48:41 +0200

    build appimage util script

---
+++ utils/build_appimage.sh
@@ -0,0 +1,8 @@
+# from https://docs.appimage.org/packaging-guide/from-source/native-binaries.html#using-the-build-system-to-build-the-basic-appdir
+# linuxdeploy and appimagetool appimage need to be in PATH
+
+cmake . -DCMAKE_INSTALL_PREFIX=/usr
+make
+make install DESTDIR=AppDir
+linuxdeploy-x86_64.AppImage --appdir AppDir -e bin/gjitenkai -d gjitenkai.desktop -i gjitenkai/kai.png
+appimagetool-x86_64.AppImage AppDir
